fd640d3d4a2f792bf7d8c4d9ced26b9018b51dba
leo2166 <lucidio2010@gmail.com>
2026-04-19 09:49:01 -0400

    chore: cambiar texto de boton de registro a Registrar Datos

--- components/FormularioRegistro.js
+++ components/FormularioRegistro.js
@@ -515,7 +515,7 @@ export default function FormularioRegistro({ isEmbedded = false }) {
                     <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                     </svg>
-                    Registrar Paciente
+                    Registrar Datos
                   </>
                 )}
               </button>
